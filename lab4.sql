776435a4dd325c961c1a00d2d0c7d86903c431c3
Maddiwallace <wallacmx@dukes.jmu.edu>
2024-09-23 17:54:50 -0400

    Update lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -5,3 +5,23 @@
 /* no, because no state has fips of 80*/
 
 --Q4.3
+/* yes, it works but can be improved*/
+
+--Q4.5
+/* yes, great job*/
+
+--Q4.6
+SELECT n.name, i.income, i.year
+FROM income i
+JOIN name n ON i.fips = n.fips
+WHERE i.year = (SELECT MAX(year) FROM income)
+ORDER BY i.income DESC
+LIMIT 1;
+
+/* yes, it works*/
+
+--Q4.7
+/* it doesn't work*/
+
+--Q4.8
+/*no, not always*/
